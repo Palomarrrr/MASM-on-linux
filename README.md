976012d3b201a34422ca8061b7a314214570f176
Palomarrrr <ieatrocks4fun123@gmail.com>
2024-02-24 19:40:35 -0600

    init to gh

---
+++ README.md
@@ -0,0 +1,16 @@
+# MASM on Linux
+I do not want to run Windows, however, my school is making us learn MASM instead of something more reasonable like NASM.
+
+This should also work on macs but I don't have one to really test it on
+## Reqirments
+- Wine 32 bit
+- A C compiler
+- Wget
+- Git
+## Setup
+Just run the script... that should be it
+
+    # ./setup.sh
+
+###### Note: 
+You may have to change the C compiler used because for some reason I couldn't get `${CC}` to work
--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 # TODO: Make a check to see if the user has...
 #   - A valid C compiler - could try to make this script just a posix sh script
+#   - wget
+#   - Git
 #   - Wine
 #   - A GNU or BSD Make system
 #   - Some kind of program to unzip the files
@@ -9,21 +11,28 @@
 echo Creating directories...
 mkdir -p ./Include ./Lib ./Tools ./Src
 
+echo Pulling MASM32 SDK...
+wget -P ./Setup/ https://masm32.masmcode.com/masm32/masm32v11r.zip
+
+echo Cloning asmbook...
+git clone https://github.com/surferkip/asmbook ./Setup/asmbook/
+
+
 # TODO: PUT A CONDITIONAL ON ALL OF THESE SO YOU AREN'T OVERWRITING SHIT
 echo Unzipping all files...
-if ! test -d ./Setup/Irvine/ ; then
-unzip ./Setup/Irvine.zip -d ./Setup/ 1> /dev/null
+if ! test -d ./Setup/asmbook/Irvine/ ; then
+unzip ./Setup/asmbook/Irvine.zip -d ./Setup/asmbook/ 1> /dev/null
 fi
 if ! test -f ./Setup/install.exe ; then
 unzip ./Setup/masm32v11r.zip -d ./Setup/ 1> /dev/null
 fi
 
 echo Moving include/library files...
-cp ./Setup/Irvine/*.inc ./Include/
-cp ./Setup/Irvine/*.asm ./Include/
-cp ./Setup/Irvine/*.lib ./Lib/
-cp ./Setup/Irvine/*.Lib ./Lib/ # Why???
-cp ./Setup/Irvine/*.obj ./Lib/
+cp ./Setup/asmbook/Irvine/*.inc ./Include/
+cp ./Setup/asmbook/Irvine/*.asm ./Include/
+cp ./Setup/asmbook/Irvine/*.lib ./Lib/
+cp ./Setup/asmbook/Irvine/*.Lib ./Lib/ # Why???
+cp ./Setup/asmbook/Irvine/*.obj ./Lib/
 
 echo Setting up tools...
 # TODO: Make this a little more dynamic if possible
@@ -38,10 +47,13 @@ export WINEDEBUG=-all # Just to make terminal output a little nicer
 wine ./Setup/install.exe 2> /dev/null
 
 echo Cleaning up...
-if test -d ./Setup/Irvine/ ; then
-rm -r ./Setup/Irvine/
+if test -d ./Setup/asmbook/ ; then
+rm -rf ./Setup/asmbook # -f Should skip all the errors
 fi
 if test -f ./Setup/install.exe ; then
+rm ./Setup/install.exe
+fi
+if test -f ./Setup/masm32v11r.zip; then
 rm ./Setup/install.exe
 fi
 
